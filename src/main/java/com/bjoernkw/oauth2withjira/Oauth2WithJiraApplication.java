d2372ff9d6c31d278979c050357270e6f3b147d6
Björn Wilmsmann <bjoern@wilmsmann.de>
2019-01-26 19:41:59 +0100

    First commit

---
+++ src/main/java/com/bjoernkw/oauth2withjira/Oauth2WithJiraApplication.java
@@ -0,0 +1,14 @@
+package com.bjoernkw.oauth2withjira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WithJiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2WithJiraApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bjoernkw/oauth2withjira/Oauth2WithJiraApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bjoernkw.oauth2withjira;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2WithJiraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
